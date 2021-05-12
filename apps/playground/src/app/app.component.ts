b60fcc49d5fac6ae19ba0a7eb1b9b6008a581c34
Jeff Cross <middlefloor@gmail.com>
2021-05-12 14:05:26 -0700

    initial commit

---
+++ apps/playground/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cacherender-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'playground';
+}
